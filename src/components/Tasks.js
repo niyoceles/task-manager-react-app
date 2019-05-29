013c00617ee89082577573c1ed641d6ac7bd2b69
Celestin NIYONSABA <niyoceles3@gmail.com>
2019-05-29 12:50:34 +0200

    Add task which are waiting to be done

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -3,7 +3,7 @@ import TaskItem from './TaskItem';
 
 class Tasks extends Component {
   render() {
-    console.log(this.props.Tasks[0].title)
+    // console.log(this.props.Tasks[0].title)
     let TaskItems;
     if (this.props.Tasks) {
       TaskItems = this.props.Tasks.map(task => {
